c814e20fd1cb2a5af904cd6847b0c4c3ee47013a
sivaprasads <somarouthu.sivaprasads@gmail.com>
2019-10-15 12:14:43 +0530

    Add Exception package

---
+++ src/main/java/com/ssp/apps/pdp/exception/DuplicateRecordException.java
@@ -0,0 +1,9 @@
+package com.ssp.apps.pdp.exception;
+
+public class DuplicateRecordException extends RuntimeException {
+
+  public DuplicateRecordException(String message) {
+    super(message);
+  }
+
+}
---
+++ src/main/java/com/ssp/apps/pdp/exception/NoRecordFoundException.java
@@ -0,0 +1,7 @@
+package com.ssp.apps.pdp.exception;
+
+public class NoRecordFoundException extends RuntimeException {
+  public NoRecordFoundException(String message) {
+    super(message);
+  }
+}
