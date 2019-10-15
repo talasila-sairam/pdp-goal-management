4c439940a8f496a52c15a7d9dc078b2729104133
sivaprasads <somarouthu.sivaprasads@gmail.com>
2019-10-15 12:07:10 +0530

    Basic Spring Boot Project Setup

---
+++ src/main/java/com/ssp/apps/pdp/PdpGoalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ssp.apps.pdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdpGoalManagementApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PdpGoalManagementApplication.class, args);
+  }
+
+}
